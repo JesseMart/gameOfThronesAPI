23b77afd6e346c6d2c56b60b75c9e94dc558f4b6
Jesse Martinez <jessemartinez452@gmail.com>
2020-02-19 10:00:17 -0600

    Displayed the sworn members of the houses, but it contains too much data

--- gameOfThronesJS.js
+++ gameOfThronesJS.js
@@ -1,24 +1,51 @@
 let listHouses = [];
 var charArr = []
-let i = 0;
-for (let p=1; p<=51;p++){
+// let i = 0;
+
+for (var p=1; p<=51;p++){
 fetch(`https://anapioficeandfire.com/api/houses?page=${p}&pageSize=50`)
 .then((response)=>{
     return response.json();
 })
 .then((houses)=>{
-    console.log(houses);
     listHouses = [...listHouses,...houses];
+    console.log(listHouses);
     let newArray = houses.map((f)=>{
-        return `<li><a class = 'link' href="${f.url}">${f.name}.</a></li>`
+        return `<li><a href="${f.url}" data-toggle='modal' data-target='#exampleModalCenter'>${f.name}.</a></li>`
     })
+    
     $('ul').append(newArray);
+    
 })
+let div = document.querySelector('div');
+div.addEventListener("click", (e)=>{
+
+    e.preventDefault();
+
+    $.get(e.target.href)
+    .done((charObj)=>{
+        let $modalBody = $('#modalBody');
+        let $modalTitle = $(".modal-title");
+        $modalTitle.html(charObj.name);
+        $modalBody.html("");
+
+        
+
+        if(charObj.swornMembers.length > 0){
+            charObj.swornMembers.forEach((charUrl)=>{
+
+                $.get(charUrl)
+                .done((charOb)=>{
+                    
+                    $modalBody.html(`${$modalBody.html()}<br>${charOb.name}`)
+                })
+            })
+        }
+        $('#exampleModalCenter').modal('show');
+    })
+
+})
+
 
 }
 
-// //house.swornMembers.forEach{
-//     something = e.split('/')
-//     index = something.pop()
-//     characters[index - 1].name
-// }
